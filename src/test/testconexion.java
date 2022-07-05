1f5c328a36831b23d5714f0f094b4e5e4d55d581
José Luis Rodríguez <107229211+JoseLuisR979@users.noreply.github.com>
2022-07-04 23:12:30 -0300

    Segundo Comit
    Se agrego crear persona, modificar persona, modificar password

--- src/test/testconexion.java
+++ src/test/testconexion.java
@@ -3,14 +3,17 @@ package test;
 
 import utilidades.conexion;
 
-public class testconexion {
+public class Testconexion {
 
 	public static void main(String[] args) {
-		conexion cnn = new conexion("root","Navidad$25","supermark");
+		conexion cnn = new conexion("super","Super1213","Supermark");
 		
 		System.out.println(cnn.conectar());
 		cnn.desconectar();
 
+		
 	}
 
+	
+	
 }
