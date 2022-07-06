2415953e4869e1245f07dc2505ecc305599d876b
claudiagerarduzzi <107524005+claudiagerarduzzi@users.noreply.github.com>
2022-07-06 00:55:02 -0300

    Update Ventas.java

--- src/ventas/Ventas.java
+++ src/ventas/Ventas.java
@@ -1,5 +1,9 @@
 package ventas;
-
 public class Ventas {
+System.out.println("1 - Crear Venta");
+System.out.println("2 - Anular Venta");
+System.out.println("3 - Ver que los usuarios realizaron una compra");
+System.out.println("4 - Ver listados elegidos por el Cliente");
+					public class Ventas {
 
 }
